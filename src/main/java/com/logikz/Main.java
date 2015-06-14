042f3ed54375eb40809e1acc93a6597953130326
Logikz <martianman@cox.net>
2015-06-13 19:22:15 -0700

    adding logs

--- src/main/java/com/logikz/Main.java
+++ src/main/java/com/logikz/Main.java
@@ -37,6 +37,6 @@ public class Main {
             System.out.println(e.getMessage());
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        } 
+        }
     }
 }
