bfc0f758a4bc2d6df84e335001b1e1ce4cf83e33
Logikz <martianman@cox.net>
2015-06-13 22:35:04 -0700

    adding files

---
+++ src/main/java/com/logikz/dao/NestDAO.java
@@ -0,0 +1,38 @@
+package com.logikz.dao;
+
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Nick on 6/13/2015.
+ */
+public class NestDAO {
+    public Response setTemperature(String token, int temperature) {
+        return null;
+    }
+
+    public void doNestAuthorization(String stateId) {
+        try {
+            OAuthClientRequest request = OAuthClientRequest
+                    .authorizationLocation("https://home.nest.com/login/oauth2")
+                    .setClientId("2f2612c7-6d36-4c5c-8c19-2903bb7be430")
+                    .setState(stateId)
+                    //.setRedirectURI("https://nest-echo.herokuapp.com/" + stateId + "/auth/callback")
+                    .buildQueryMessage();
+            String locationUri = request.getLocationUri();
+            System.out.println(locationUri);
+            Response response = ClientBuilder.newClient().target(locationUri).request().buildGet().invoke();
+
+            System.out.println(response.getEntity());
+        } catch (OAuthSystemException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String getToken(String body) {
+        return null;
+    }
+}
---
+++ src/main/java/com/logikz/dao/PostgresDAO.java
@@ -0,0 +1,39 @@
+package com.logikz.dao;
+
+import com.logikz.utils.PostgresConnection;
+
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by Nick on 6/13/2015.
+ */
+public class PostgresDAO {
+
+    public PostgresDAO() {
+    }
+
+    public void setToken(String stateId, String token) throws URISyntaxException, SQLException {
+        try(Connection connection = PostgresConnection.getConnection()){
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS nest (stateId VARCHAR(50), token VARCHAR(500))");
+            statement.executeUpdate("INSERT INTO nest VALUES (" + stateId + ", " + token + ")");
+        }
+    }
+
+    public String getToken(String stateId) throws URISyntaxException, SQLException {
+        try(Connection connection = PostgresConnection.getConnection()){
+            Statement statement = connection.createStatement();
+
+            try(ResultSet resultSet = statement.executeQuery("SELECT token FROM nest WHERE stateId=" + stateId)){
+                if(resultSet.next()){
+                    return resultSet.getString(0);
+                }
+            }
+        }
+        return null;
+    }
+}
